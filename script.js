99e179a573cac41dd598e393a85d1b2dee08ef59
Edward Boguslavsky <eb.contactme@gmail.com>
2026-09-25 11:32:20 -0400

    Created custom scroll bar

--- script.js
+++ script.js
@@ -1,31 +1,55 @@
 $(document).ready(function() {
-    var $follower = $('.tooltip');
+    var $highlight = $('.highlight');
+    var $tooltip = $('.tooltip');
     var gap = 0
     var date = new Date();
     
-    $follower.hide();
+    $tooltip.hide();
   
     $('.ascii > a').on({
-      mouseenter: function() {
-        $follower.show();
-        date = new Date();
-      },
-      mouseleave: function() {
-        $follower.hide();
-      },
-      mousemove: function(e) {
-        $follower.css({
-          left: e.pageX - $follower.outerWidth() - gap,
-          top: e.pageY - $follower.outerHeight() - gap
-        });
-      }
+        mouseenter: function() {
+            $tooltip.show();
+            date = new Date();
+        },
+        mouseleave: function() {
+            $tooltip.hide();
+        },
+        mousemove: function(e) {
+            $tooltip.css({
+                left: e.pageX - $tooltip.outerWidth() - gap,
+                top: e.pageY - $tooltip.outerHeight() - gap
+            });
+        }
     });
 
+    $("body").on({
+        mousemove: function(e) {
+            $highlight.css({
+                top: Math.trunc(e.pageY / $highlight.height()) * $highlight.height()
+            });
+        }
+    });
 
     $(".tooltip > .time").each(function() {
         $(this).text(date.getHours() + ":" + ('0' + date.getMinutes()).slice(-2));
     });
   });
+
+$(window).on("scroll", function() {
+    var scroll_top = $(window).scrollTop();
+    var doc_height = $(document).height();
+    var win_height = $(window).height();
+    
+    var scroll_percent = (scroll_top / (doc_height - win_height)) * 100;
+    
+    $(".scrollbar > span").css("top", scroll_percent + "%");
+
+    if (scroll_percent <= 0.1 || scroll_percent >= 99.9) {
+        $(".scrollbar > span").css("color", "var(--rose-600)");
+    } else {
+        $(".scrollbar > span").css("color", "var(--true-gray-400)");
+    }
+});
 
 $(function () {
     const $sections = $("section");
